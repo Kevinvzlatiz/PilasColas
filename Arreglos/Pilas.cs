9741f2b4b66fecca5849f0a61de50a71f8524fa5
ACER <ACER@DESKTOP-L4GTLHN>
2025-05-24 13:33:39 -0600

    Agregar archivos de proyecto.

---
+++ Arreglos/Pilas.cs
@@ -0,0 +1,22 @@
+﻿namespace Arreglos
+{
+    public class Pilas
+    {
+        private string[] _arreglo;
+        private int _tope;
+        private int _max;
+
+        public Pilas(int elementos)
+        {
+            _arreglo = new string[elementos];
+            _tope = 0;
+            _max = _arreglo.Length -1;
+        }
+
+        public void Agregar(string dato) 
+        { 
+            _arreglo[_tope] = dato;
+            _tope++;
+        }
+    }
+}
---
+++ PilasColas/Program.cs
@@ -0,0 +1,13 @@
+﻿using Arreglos;
+namespace PilasColas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pilas pila = new Pilas(5);
+            pila.Agregar("A");
+            pila.Agregar("B");
+        }
+    }
+}
