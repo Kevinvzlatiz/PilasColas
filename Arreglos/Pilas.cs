a139edd4ba60356277b2b2fe32fcb41d64e8b91e
Kevin Valenzuela <kevinvalenzuela@MacBook-Air-de-Kevin.local>
2025-05-31 13:17:47 -0700

    agregamos el comportamiento de elminar e imprimir

--- Arreglos/Pilas.cs
+++ Arreglos/Pilas.cs
@@ -10,13 +10,40 @@
         {
             _arreglo = new string[elementos];
             _tope = 0;
-            _max = _arreglo.Length -1;
+            _max = _arreglo.Length - 1;
         }
 
-        public void Agregar(string dato) 
-        { 
+        public void Agregar(string dato)
+        {
+            if (_tope > _max)
+            {
+                throw new Exception("El arreglo está lleno");
+            }
+
             _arreglo[_tope] = dato;
             _tope++;
+        }
+
+        public void Eliminar()
+        {
+            if (_tope < 1)
+            {
+                throw new Exception("No hay nada que eliminar");
+            }
+
+            _tope--;
+            _arreglo[_tope] = "";
+
+        }
+
+        public string Imprimir()
+        {
+            string datos = string.Empty;
+            for (int i = 0; i < _tope; i++)
+            {
+                datos += _arreglo[i];
+            }
+            return datos;
         }
     }
 }
